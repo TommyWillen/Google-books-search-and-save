179ace253cb6ef793499838962296d8ae6f5b1ae
Tommy Willen <tommyallen1215@gmail.com>
2021-02-11 08:56:39 -0800

    fixed search issues

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button"
 
 function SearchForm(props) {
-  return (
+    const [searchState, setSearchState] = useState("");
+
+    function handleInputChange(event) {
+        const value = event.target.value;
+        setSearchState(value);
+      };
+    
+      function handleFormSubmit (event) {
+        event.preventDefault();
+        props.searchBook(searchState);
+        setSearchState("")
+      };
+
+    return (
     <Container>
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Form.Group>
           <Form.Label htmlFor="search">
             <h2>Search for and save Books of Interest</h2>
@@ -16,14 +29,15 @@ function SearchForm(props) {
             placeholder="Search for a book"
             id="search"
             name="search"
-            onChange={props.handleInputChange}
-            value={props.search}
+            onChange={handleInputChange}
+            value={searchState}
           ></Form.Control>
           
           <Button
-            onClick={props.handleFormSubmit}
+            // onClick={props.handleFormSubmit}
             variant="dark"
             className="mt-3 mb-5"
+            type="submit"
           >
             Search
           </Button>
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,57 +2,54 @@ import React, { useState } from "react";
 import SearchForm from "../components/SearchForm";
 import Results from "../components/Results";
 import API from "../utils/API";
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
 
 function Search() {
-  const [bookState, setBookState] = useState({
-    search: "",
-    books: [],
-  });
+  const [bookState, setBookState] = useState([]);
 
-  function makeBook(bookData)  {
+  function makeBook(bookData) {
+    console.log(bookData.volumeInfo.imageLinks);
     return {
       _id: bookData.id,
       title: bookData.volumeInfo.title,
       authors: bookData.volumeInfo.authors,
       description: bookData.volumeInfo.description,
-      image: bookData.volumeInfo.imageLinks.thumbnail,
+      image: bookData.volumeInfo.imageLinks?.thumbnail || "No Image Found",
       link: bookData.volumeInfo.previewLink,
     };
-  };
+  }
 
-  function searchBook (query) {
+  function searchBook(query) {
+    console.log(query);
     API.getBook(query)
-      .then((res) =>
-        setBookState({
-          books: res.data.items.map((bookData) => makeBook(bookData)),
-        })
-      )
+      .then((res) => {
+        setBookState(res.data.items.map((bookData) => makeBook(bookData)));
+      })
       .catch((err) => console.error(err));
-  };
+  }
 
-  function handleInputChange(event) {
-    console.log(event.target)
-    const value = event.target.value;
-    setBookState({
-      search: value,
-    });
-  };
+  // function handleInputChange(event) {
+  //   const value = event.target.value;
+  //   setBookState({
+  //     search: value,
+  //   });
+  // };
 
-  function handleFormSubmit (event) {
-    event.preventDefault();
-    searchBook(bookState.search);
-  };
+  // function handleFormSubmit (event) {
+  //   event.preventDefault();
+  //   searchBook(bookState.search);
+  // };
 
   return (
     <div>
       <SearchForm
-        handleInputChange={handleInputChange}
-        handleFormSubmit={handleFormSubmit}
+        searchBook={searchBook}
+        // handleInputChange={handleInputChange}
+        // handleFormSubmit={handleFormSubmit}
       />
       <Container>
         <h2>Results</h2>
-        <Results books={bookState.books} />
+        <Results books={bookState} />
       </Container>
     </div>
   );
